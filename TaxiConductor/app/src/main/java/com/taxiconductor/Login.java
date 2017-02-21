d71da4ea6d3acd6b41337975a5c182440f6a6761
Lichthammer <asandovalg1993@gmail.com>
2017-02-21 11:10:12 -0600

    Login e inicio
    Hasta aquí login e inicio

---
+++ TaxiConductor/app/src/main/java/com/taxiconductor/Login.java
@@ -0,0 +1,29 @@
+package com.taxiconductor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+    private EditText et_email;
+    private EditText et_password;
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Home.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
